660b60467612a73fafdfabc5b152b1bbf9621010
Favilla11 <66454333+Favilla11@users.noreply.github.com>
2021-12-07 01:05:19 -0500

    Update MarkAdapter.java

--- src/main/java/com/example/myfirstapp/MarkAdapter.java
+++ src/main/java/com/example/myfirstapp/MarkAdapter.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.owner_ordersactivity.R;
+
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.ArrayList;
