c59f9ac0025f9a25a854d8ec229ea72871ce9fee
Favilla11 <66454333+Favilla11@users.noreply.github.com>
2021-12-07 01:06:20 -0500

    Update Mark_OrdersActivity.java

--- src/main/java/com/example/myfirstapp/Mark_OrdersActivity.java
+++ src/main/java/com/example/myfirstapp/Mark_OrdersActivity.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.owner_ordersactivity.R;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
